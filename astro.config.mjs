0217541303323b4f226be58ee7408251fdb865db
Ismytv <ismytv@gmail.com>
2024-09-28 19:38:10 -0500

    chore:add @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 
+import cloudflare from '@astrojs/cloudflare';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  
-
+  output: 'hybrid',
+  adapter: cloudflare(),
 });
