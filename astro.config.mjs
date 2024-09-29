42a2c2831c32ee7bb547ded58429e453aef9f775
Ismytv <ismytv@gmail.com>
2024-09-28 19:35:26 -0500

    chore:remove @astrojs/node

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,11 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   
-  output: 'hybrid',
 
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
